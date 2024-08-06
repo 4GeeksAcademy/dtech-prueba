d09028e5b35cedeaccb9395997cfad5a33ab863c
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-08-06 00:51:14 +0000

    BACK - Added classes Post and Post-likes

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
@@ -11,9 +12,10 @@ class User(db.Model):
     avatar = db.Column(db.String(120), unique=True, nullable=True)
 
     posts = db.relationship('Post', backref='author', lazy=True)
+    likes = db.relationship('PostLike', backref='user', lazy=True)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.id}>'
 
     def serialize(self):
         return {
@@ -22,5 +24,47 @@ class User(db.Model):
             "name": self.name,
             "surname": self.surname,
             "avatar": self.avatar
-            # do not serialize the password, its a security breach
+        }
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String(255), nullable=False)
+    message = db.Column(db.String(500), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    location = db.Column(db.String(30), nullable=False)
+    status = db.Column(db.String(10), nullable=False, default='drafted')
+
+    likes = db.relationship('PostLike', backref='post', lazy=True)
+
+    def __repr__(self):
+        return f'<Post {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "image": self.image,
+            "message": self.message,
+            "author": self.author.serialize(),
+            "created_at": self.created_at,
+            "location": self.location,
+            "status": self.status,
+            "likes": [like.user.serialize() for like in self.likes]
+        }
+
+
+class PostLike(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return f'<PostLike {self.user_id} likes {self.post_id}>'
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "post_id": self.post_id,
+            "timestamp": self.timestamp
         }
