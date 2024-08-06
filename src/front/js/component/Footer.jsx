93e7e5a65af55c048023f1887a56a448c43a557c
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-08-06 15:09:50 +0000

    Changed some text at Navbar and footer

--- src/front/js/component/Footer.jsx
+++ src/front/js/component/Footer.jsx
@@ -3,6 +3,6 @@ import { PiAlienLight } from "react-icons/pi";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-			<h6 ><PiAlienLight />A product by <strong>Digital Tech Inc.</strong></h6>
+			<h6 ><PiAlienLight />MyInstagramAPP is a product by <strong>Digital Tech Inc.</strong></h6>
 	</footer>
 );
